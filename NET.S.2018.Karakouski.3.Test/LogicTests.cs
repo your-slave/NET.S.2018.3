16eac2489dd18cc3253e11ea3ca8a8b75aa0c0d6
u <u@UNO>
2018-03-19 18:59:50 +0300

    Fixed test

--- NET.S.2018.Karakouski.3.Test/LogicTests.cs
+++ NET.S.2018.Karakouski.3.Test/LogicTests.cs
@@ -7,16 +7,16 @@ namespace NET.S._2018.Karakouski.NUnit.Tests
     [TestFixture]
     public class LogicTests
     {
-        [TestCase(1, 5, 0.0001, ExpectedResult = 1)]
-        [TestCase(1, 5, 0.0001, ExpectedResult = 1)]
-        [TestCase(8, 3, 0.0001, ExpectedResult = 2)]
-        [TestCase(0.001, 3, 0.0001, ExpectedResult = 0.1)]
-        [TestCase(0.04100625, 4, 0.0001, ExpectedResult = 0.45)]
-        [TestCase(8, 3, 0.0001, ExpectedResult = 2)]
-        [TestCase(0.0279936, 7, 0.0001, ExpectedResult = 0.6)]
-        [TestCase(0.0081, 4, 0.1, ExpectedResult = 0.3)]
-        [TestCase(-0.008, 3, 0.1, ExpectedResult = -0.2)]
-        [TestCase(0.004241979, 9, 0.00000001, ExpectedResult = 0.545)]
+        [TestCase(1, 5, 0.0001, 1, .005)]
+        [TestCase(1, 5, 0.0001, 1, .005)]
+        [TestCase(8, 3, 0.0001, 2, .005)]
+        [TestCase(0.001, 3, 0.0001, 0.1, .005)]
+        [TestCase(0.04100625, 4, 0.0001, 0.45, .005)]
+        [TestCase(8, 3, 0.0001, 2, .005)]
+        [TestCase(0.0279936, 7, 0.0001, 0.6, .005)]
+        [TestCase(0.0081, 4, 0.1, 0.3, .005)]
+        [TestCase(-0.008, 3, 0.1, 0.2, .005)]
+        [TestCase(0.004241979, 9, 0.00000001, 0.545, .005)]
         public double FindNthRoot_TestCalulations(double number, int root, double tolerance) => Logic.FindNthRoot(number, root, tolerance);
 
         [TestCase]
